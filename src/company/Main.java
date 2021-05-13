a5005ff6d6f4efc99e92354357e530783ae939fe
Pablo Pereira Moure <ppereiramoure@danielcastelao.com>
2021-05-13 17:21:08 +0200

    Libreria Importada

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,8 +1,22 @@
 package company;
 
+import libreriasv12.FactoriaDeSalidas;
+import libreriasv12.InterfaceMetodosComunes;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        InterfaceMetodosComunes miSalida;
+        InterfaceMetodosComunes salidaVentana;
+        InterfaceMetodosComunes salidaImpresora;
+
+        miSalida = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.CONSOLA,"Hola desde la fabrica");
+        miSalida.visualiza();
+
+        salidaVentana = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.VENTANA,"Hola desde la fabrica");
+        salidaVentana.visualiza();
+
+        salidaImpresora = FactoriaDeSalidas.getProducto (FactoriaDeSalidas.IMPRESORA,"Hola desde la fabrica");
+        salidaImpresora.visualiza();
     }
 }
